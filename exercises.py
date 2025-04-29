4e8974182736de0c507df246e846cc30f3529372
Bri Pintens <btpintens@gmail.com>
2025-04-29 16:44:37 -0400

    up to 4.5

--- exercises.py
+++ exercises.py
@@ -42,9 +42,48 @@ print('Exercise 2:', simple_interest(1000, 5, 2))
 #
 # Define your function and call it to display the discounted price.
 def apply_discount(price, discount):
-    new_price = (price * (discount / 100))
+    new_price = price - (price/price * discount)
     return new_price
 
 
 print('Exercise 3:', apply_discount(100, 25))
 
+# Exercise 4: Convert Temperature
+#
+# Write a function called `convert_temperature` that takes a
+# temperature and a unit ('C' for Celsius, 'F' for Fahrenheit)
+# and converts the temperature to the other unit.
+# The formula for converting Celsius to Fahrenheit is (Celsius * 9/5) + 32.
+# The formula for converting Fahrenheit to Celsius is (Fahrenheit - 32) * 5/9.
+#
+# Examples:
+# convert_temperature(0, 'C') should return 32.0.
+# convert_temperature(32, 'F') should return 0.0.
+#
+# Define the function and then call it below.
+def convert_temperature(temperature, ):
+    conversion = (temperature * 9/5) + 32
+    return conversion, 
+
+def convert_temperature(temperature, ):
+    conversion = (temperature - 32) * 5/9
+    return conversion, 
+
+
+print('Exercise 4: Convert 0°C to Fahrenheit:', convert_temperature(0, 'C'))
+print('Exercise 4: Convert 32°F to Celsius:', convert_temperature(32, 'F'))
+
+
+# Exercise 5: Sum to N
+#
+# Write a function named `sum_to` that takes a single integer n and returns the sum of all integers from 1 to n.
+#
+# Examples:
+# sum_to(6) should return 21.
+# sum_to(10) should return 55.
+#
+# Define the function and then call it below.
+
+
+
+print('Exercise 5:', sum_to(6))
